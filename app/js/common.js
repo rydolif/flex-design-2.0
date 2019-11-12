57a9429650d2179f4833cd490f5b5706d361b537
Mykola Polishchuk <rudolifrudolif@gmail.com>
2019-11-12 22:59:42 +0200

    init commit

---
+++ app/js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+
+//-------------------------------активна ссилка на якій знаходишся для меню---------------------------------------
+	var swiper = new Swiper('.hero__slider', {
+	  spaceBetween: 0,
+	  effect: 'fade',
+	  autoplay: {
+	    delay: 4500,
+	    disableOnInteraction: false,
+	  },
+	});
+
+
+
+});
+
